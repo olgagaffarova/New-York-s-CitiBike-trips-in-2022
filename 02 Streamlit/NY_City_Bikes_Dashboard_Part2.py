d1191fd91944711e31deaaf37865df895a4e125c
Olga Gaffarova <116673670+olgagaffarova@users.noreply.github.com>
2025-11-12 20:50:45 +0100

    NY_City_Bikes_Dashboard_Part2.py

--- 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
+++ 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
@@ -53,7 +53,7 @@ hours_member_casual = pd.read_csv('02 Streamlit/hours_member_casual.csv', index_
 monthly_waterfront = pd.read_csv('02 Streamlit/monthly_waterfront.csv', index_col=0)
 waterfront_trips = pd.read_csv('02 Streamlit/waterfront_trips.csv', index_col=0)
 monthly_type = pd.read_csv('02 Streamlit/fleet_reduction.csv', index_col=0)
-low_season_summary = pd.read_csv('02 Streamlit/low_season_summary.csv', index_col=0)
+low_season = pd.read_csv('02 Streamlit/low_season_summary.csv', index_col=0)
 
 month_order = [
     'January', 'February', 'March', 'April', 'May', 'June',
