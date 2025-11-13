1bd584581d7988ed8c35c834162f18f65d38d7b4
Olga Gaffarova <116673670+olgagaffarova@users.noreply.github.com>
2025-11-13 14:33:27 +0100

    NY_City_Bikes_Dashboard_Part2.py

--- 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
+++ 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
@@ -56,6 +56,15 @@ low_season = pd.read_csv('02 Streamlit/low_season_summary.csv', index_col=0)
 low_season_type = pd.read_csv('02 Streamlit/low_season_type.csv', index_col=0)
 popular_stations = pd.read_csv('02 Streamlit/popular_stations.csv', index_col=0)
 
+@st.cache_data
+def get_data():
+    url = "https://drive.google.com/uc?id=1ai7N88esxAunuUDLXErQE2S7QlaePgV7"
+    return pd.read_csv(url)
+
+popular_stations = get_data()
+st.dataframe(popular_stations.head())
+
+
 
 month_order = [
     'January', 'February', 'March', 'April', 'May', 'June',
