57a7453c833b04404afa261e874abab4d881e17a
Olga Gaffarova <116673670+olgagaffarova@users.noreply.github.com>
2025-11-14 10:08:44 +0100

    NY_City_Bikes_Dashboard_Part2.py

--- 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
+++ 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
@@ -85,8 +85,7 @@ if page == "About Citibike":
 
     st.markdown("""
     #### Top 15 Most Popular Start Stations
-    The following chart highlights the **15 busiest start stations**, mostly in **central Manhattan**, 
-    where daily commuter demand is highest.
+    The following chart highlights the **15 busiest start stations**, mostly in **central Manhattan**, where daily commuter demand is highest.
 	- Busiest stations are concentrated in **central Manhattan** commuter and tourist zones.
 	- Top stations (e.g., *W 21 St & 6 Ave*) exceed **120k annual starts**.
     """)
