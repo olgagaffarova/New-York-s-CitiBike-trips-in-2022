31052d7cdd263bbade2f761a507e9f2631be6b1d
Olga Gaffarova <116673670+olgagaffarova@users.noreply.github.com>
2025-11-14 12:26:37 +0100

    NY_City_Bikes_Dashboard_Part2.py

--- 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
+++ 02 Streamlit/NY_City_Bikes_Dashboard_Part2.py
@@ -755,6 +755,33 @@ elif page == "Waterfront Expansion Opportunities":
     st.markdown("""
     Explore **spatial demand clusters** along **Hudson and East River** to identify where 
     new stations could relieve congestion and serve leisure riders.
+    **Objective**:
+    Our approach focuses mainly on places where people are able to access the waterline. 
+    To identify locations where new bike stations can relieve congestion and serve leisure riders.
+    
+    **Methodology**:
+    
+    **Define Waterfront Zones by using Waterfront Access Plans (WAPs)** with focus only on zones with legal public access for station placement:
+    Hunters Point
+    Newtown Creek
+    Harlem River – Core
+    Inwood
+    Harlem River – North
+    Harlem River – South
+    Flushing
+    Gowanus Canal
+    Supply-Demand Gap Analysis
+    
+    **Supply** : Percentage of existing stations located in these WAP zones and within 400 meters of the shoreline are considered waterfront stations.
+    **Demand**: Percentage of trips that start or end in these zones
+    **Gap**: Areas where demand exceeds supply are underserved and ideal for new stations
+    
+    Peak Month Analysis
+    Identify months with highest waterfront demand to guide expansion timing
+        
+    
+    
+        
     """)
 
     # ============================================================================
